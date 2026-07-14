4a4904ee8361bd2e5a7c460165ec6066163e8750
hari-kiran-pasumarthi <142865072+hari-kiran-pasumarthi@users.noreply.github.com>
2026-07-15 00:59:43 +0530

    Add WhyKamalavasinii component to Home page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import SiteHeader from "@/components/layout/SiteHeader";
 import SiteFooter from "@/components/layout/SiteFooter";
 import Hero from "@/components/home/Hero";
 import CuratedTreasures from "@/components/home/CuratedTreasures";
+import WhyKamalavasinii from "@/components/home/WhyKamalavasinii";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <Hero />
 
       <CuratedTreasures />
+
+      <WhyKamalavasinii />
 
       <SiteFooter />
     </>
