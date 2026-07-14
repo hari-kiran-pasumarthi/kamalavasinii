275f41dbf068955e298d80fe4e93b006f463f01f
hari-kiran-pasumarthi <142865072+hari-kiran-pasumarthi@users.noreply.github.com>
2026-07-15 01:02:46 +0530

    Add Heritage component to Home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import SiteHeader from "@/components/layout/SiteHeader";
 import SiteFooter from "@/components/layout/SiteFooter";
+
 import Hero from "@/components/home/Hero";
 import CuratedTreasures from "@/components/home/CuratedTreasures";
 import WhyKamalavasinii from "@/components/home/WhyKamalavasinii";
+import Heritage from "@/components/home/Heritage";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <CuratedTreasures />
 
       <WhyKamalavasinii />
+
+      <Heritage />
 
       <SiteFooter />
     </>
