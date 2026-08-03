893bdf9ed41e9062c7544b05e7d2b7e7dfbdb874
hari-kiran-pasumarthi <142865072+hari-kiran-pasumarthi@users.noreply.github.com>
2026-08-03 10:55:10 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,44 @@ import { SiteHeader, SiteFooter } from "@/components/SiteChrome";
 
 import Hero from "@/components/home/Hero";
 import FeaturedCategories from "@/components/home/FeaturedCategories";
+import Collections from "@/components/home/Collections";
 import CuratedTreasures from "@/components/home/CuratedTreasures";
 import WhyKamalavasinii from "@/components/home/WhyKamalavasinii";
 import Heritage from "@/components/home/Heritage";
 import CustomJourney from "@/components/home/CustomJourney";
 import Testimonials from "@/components/home/Testimonials";
 import CTA from "@/components/home/CTA";
-import Collections from "@/components/home/Collections";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#FDF7EF]">
       <SiteHeader activeHref="/" />
 
+      {/* Hero */}
       <Hero />
 
-      {/* New Section */}
+      {/* Featured Categories */}
       <FeaturedCategories />
 
+      {/* Collections */}
+      <Collections />
+
+      {/* Curated Treasures */}
       <CuratedTreasures />
+
+      {/* Why Kamalavasinii */}
       <WhyKamalavasinii />
+
+      {/* Heritage */}
       <Heritage />
+
+      {/* Custom Journey */}
       <CustomJourney />
+
+      {/* Testimonials */}
       <Testimonials />
+
+      {/* Call To Action */}
       <CTA />
 
       <SiteFooter />
